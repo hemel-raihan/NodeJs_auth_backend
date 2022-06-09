0aafefaf42a562a8ce371f4aa1a9a59e0dd8c08f
hemel-raihan <hemelraihan1997@gmail.com>
2022-06-09 12:31:51 +0600

    done login with jwt token in cookie & fetched user

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =  require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 mongoose.connect('mongodb+srv://hemelsite:r3w4DU4hIayHzGZb@cluster0.khxme.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -11,8 +13,15 @@ mongoose.connect('mongodb+srv://hemelsite:r3w4DU4hIayHzGZb@cluster0.khxme.mongod
 const routes = require('./routes/routes')
 
 app = express()
+
+app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}))
+
 app.use(express.json())
 
 app.use('/api', routes)
 
-app.listen(8003);
+app.listen(8000);
