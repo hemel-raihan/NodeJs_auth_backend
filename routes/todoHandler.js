7688a8abe2fa560059f5dae6298c5edaa14a7ef9
hemel-raihan <hemelraihan1997@gmail.com>
2022-06-09 18:27:37 +0600

    fetched all todo data & single todo data

--- routes/todoHandler.js
+++ routes/todoHandler.js
@@ -6,14 +6,62 @@ const Todo = new mongoose.model("Todo", todoSchema)
 
 //get all todo
 
-router.get('/', async (req,res) =>{
+// router.get('/', async (req,res) =>{
+//      Todo.find({status: 'inactive'}, (err,data)=>{
+//         if(err)
+//         {
+//             res.status(500).json({
+//                 error: "there is some wrong..!!!"
+//             })
+//         }
+//         else{
+//             res.status(200).json({
+//                 result: data,
+//                 message: "successfully fetched",
+//             })
+//         }
+//     })
+// })
 
-})
+router.get("/", async (req, res) => {
+    await Todo.find({ status: "active" })
+      .select({
+        _id: 0,
+        __v: 0,
+        date: 0,
+      })
+      .limit(2)
+      .exec((err, data) => {
+        if (err) {
+          res.status(500).json({
+            error: "There was a server side error!",
+          });
+        } else {
+          res.status(200).json({
+            result: data,
+            message: "Success",
+          });
+        }
+      });
+  });
 
 //get single todo
 
 router.get('/:id', async (req,res) =>{
-
+    Todo.find({_id: req.params.id}, (err,data)=>{
+    if(err)
+    {
+        res.status(500).json({
+            error: "there is some wrong..!!!"
+        })
+    }
+    else{
+        res.status(200).json({
+            result: data,
+            message: "successfully fetched",
+        })
+    }
+})
 })
 
 // insert a todo
@@ -85,7 +133,7 @@ router.put("/:id", async (req, res) => {
             { _id: req.params.id },
             {
               $set: {
-                status: "active",
+                status: "inactive",
               },
             },
             {
@@ -103,7 +151,9 @@ router.put("/:id", async (req, res) => {
         });
     }
     
-  });
+  })
+
+
 
 router.delete('/:id', async (req,res) =>{
 
